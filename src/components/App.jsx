2c6184c827bddbc13e1d1db2eb0159c529cc0cad
Heorhii Honcharov <0019@ukr.net>
2023-06-13 15:38:20 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { MainContainer } from './MainContainer';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  incrementField = option => {
+    this.setState(pState => {
+      return {
+        [option]: pState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = state => {
+    return Object.values(state).reduce((total, item) => total + item, 0);
+  };
+
+  countPositiveFeedbackPercentage = (total, { good }) => {
+    return Math.round((good / total) * 100);
+  };
+
+  render() {
+    return (
+      <MainContainer>
+        <GlobalStyle />
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            incrementField={this.incrementField}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback(this.state) ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback(this.state)}
+              positivePercentage={this.countPositiveFeedbackPercentage(
+                this.countTotalFeedback(this.state),
+                this.state
+              )}
+            ></Statistics>
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </MainContainer>
+    );
+  }
+}
