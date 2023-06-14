1b152d297614a997c4d252d681246b34926fd9bf
Heorhii Honcharov <0019@ukr.net>
2023-06-14 20:20:55 +0200

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { MainContainer } from './MainContainer';
 import { Section } from './Section/Section';
@@ -10,28 +10,24 @@ export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
 
   const handleIncrement = item => {
     switch (item) {
       case 'good':
-        setGood(good => good + 1);
+        setGood(item => item + 1);
         break;
       case 'neutral':
-        setNeutral(neutral => neutral + 1);
+        setNeutral(item => item + 1);
         break;
       case 'bad':
-        setBad(bad => bad + 1);
+        setBad(item => item + 1);
         break;
       default:
         break;
     }
   };
 
-  useEffect(() => {
-    setTotal(good + neutral + bad);
-  }, [bad, neutral, good]);
-
+  const total = good + neutral + bad;
   const positivePercentage = Math.round(100 * (good / total));
 
   return (
